cec1d38e08ef1af3b1c3e3c535b6aac0210d5dfa
SG2Calendar <sg2calendario@gmail.com>
2019-12-01 13:17:51 +0000

    Update data.js

--- data.js
+++ data.js
@@ -601,19 +601,19 @@ var jsonData = {
             {
                 "d": 23,
                 "e": [
-                {
-                    "i": "https://i.imgur.com/zGm6lA5.png",
-                    "u": "https://i.imgur.com/9BgHnmP.jpg",
-                    "g": "https://www.sg2global.com/forum/index.php?thread/367-event-moonlight-treasure-box/",
-                    "t": "All Day",
-                    "n": "Moonlight Box"
-                },
-                {
-                    "i": "./images/logo.png",
-                    "g": "",
-                    "t": "17:00",
-                    "n": "Nation War"
-                }]
+                    {
+                        "i": "https://i.imgur.com/zGm6lA5.png",
+                        "u": "https://i.imgur.com/9BgHnmP.jpg",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/367-event-moonlight-treasure-box/",
+                        "t": "All Day",
+                        "n": "Moonlight Box"
+                    },
+                    {
+                        "i": "./images/logo.png",
+                        "g": "",
+                        "t": "17:00",
+                        "n": "Nation War"
+                    }]
             },
             {
                 "d": 24,
@@ -690,6 +690,329 @@ var jsonData = {
                     "t": "All Day",
                     "n": "Moonlight Box"
                 }]
+            }
+        ]
+    },
+    "112019": {
+        "y": 2019,
+        "m": 11,
+        "som": "https://i.imgur.com/Q45bywt.jpg",
+        "tc": "BB2528",
+        "ec": "F8B229",
+        "days": [
+            {
+                "d": 1,
+                "i": "https://i.imgur.com/5HTFviS.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/372-event-mistery-box/",
+                "t": "All Day",
+                "n": "Mystery Box",
+                "u": "https://i.imgur.com/kB9HwLy.png"
+            },
+            {
+                "d": 2,
+                "i": "https://i.imgur.com/M015dld.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/365-event-dt-run/",
+                "t": "17:00",
+                "n": "DT Run",
+                "u": "https://i.imgur.com/l9rRJLi.jpg"
+            },
+            {
+                "d": 3,
+                "i": "https://i.imgur.com/IllUyVq.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                "t": "17:00",
+                "n": "OX Event",
+                "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                "f": 0
+            },
+            {
+                "d": 4,
+                "i": "https://i.imgur.com/S25ARD8.png",
+                "u": "https://i.imgur.com/aQ059WE.jpg",
+                "g": "https://www.sg2global.com/forum/index.php?thread/394-event-metin-spawn/",
+                "t": "17:00",
+                "n": "Metin Spawn"
+            },
+            {
+                "d": 5,
+                "e": [
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "15:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    },
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "17:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    }]
+            },
+            {
+                "d": 6,
+                "i": "./images/logo.png",
+                "g": "",
+                "t": "17:00",
+                "n": "Hunter Hour"
+            },
+            {
+                "d": 7,
+                "i": "https://i.imgur.com/zGm6lA5.png",
+                "u": "https://i.imgur.com/9BgHnmP.jpg",
+                "g": "https://www.sg2global.com/forum/index.php?thread/367-event-moonlight-treasure-box/",
+                "t": "All Day",
+                "n": "Moonlight Box"
+            },
+            {
+                "d": 8,
+                "e": [{
+                    "i": "https://i.imgur.com/nnJgE4E.png",
+                    "g": "https://www.sg2global.com/forum/index.php?thread/551-event-hexagonal-box/",
+                    "t": "All Day",
+                    "n": "Hexagonal Box",
+                    "u": "https://i.imgur.com/kgPPMkZ.jpg"
+                }, {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "17:00",
+                    "n": "Siege War"
+                }]
+            },
+            {
+                "d": 9,
+                "i": "https://i.imgur.com/dAm7jFf.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/326-event-mining-party/",
+                "t": "17:00",
+                "n": "Mining Party",
+                "u": "https://i.imgur.com/JOiieaI.jpg",
+                "f": 0
+            },
+            {
+                "d": 10,
+                "i": "https://i.imgur.com/IllUyVq.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                "t": "17:00",
+                "n": "OX Event",
+                "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                "f": 0
+            },
+            {
+                "d": 11,
+                "i": "https://i.imgur.com/M015dld.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/365-event-dt-run/",
+                "t": "17:00",
+                "n": "DT Run",
+                "u": "https://i.imgur.com/l9rRJLi.jpg"
+            },
+            {
+                "d": 12,
+                "e": [
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "15:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    },
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "17:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    }]
+            },
+            {
+                "d": 13,
+                "i": "./images/logo.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/562-event-boss-spawn/",
+                "t": "20:00",
+                "n": "Boss Spawn"
+            },
+            {
+                "d": 14,
+                "i": "https://i.imgur.com/zGm6lA5.png",
+                "u": "https://i.imgur.com/9BgHnmP.jpg",
+                "g": "https://www.sg2global.com/forum/index.php?thread/367-event-moonlight-treasure-box/",
+                "t": "All Day",
+                "n": "Moonlight Box"
+            },
+            {
+                "d": 15,
+                "i": "https://i.imgur.com/5HTFviS.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/372-event-mistery-box/",
+                "t": "All Day",
+                "n": "Mystery Box",
+                "u": "https://i.imgur.com/kB9HwLy.png"
+            },
+            {
+                "d": 16,
+                "i": "https://i.imgur.com/M015dld.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/365-event-dt-run/",
+                "t": "17:00",
+                "n": "DT Run",
+                "u": "https://i.imgur.com/l9rRJLi.jpg"
+            },
+            {
+                "d": 17,
+                "i": "https://i.imgur.com/IllUyVq.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                "t": "17:00",
+                "n": "OX Event",
+                "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                "f": 0
+            },
+            {
+                "d": 18,
+                "i": "https://i.imgur.com/S25ARD8.png",
+                "u": "https://i.imgur.com/aQ059WE.jpg",
+                "g": "https://www.sg2global.com/forum/index.php?thread/394-event-metin-spawn/",
+                "t": "17:00",
+                "n": "Metin Spawn"
+            },
+            {
+                "d": 19,
+                "e": [
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "15:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    },
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "17:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    }]
+            },
+            {
+                "d": 20,
+                "i": "https://i.imgur.com/CX7RHb8.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/553-event-community-event/",
+                "t": "17:00",
+                "n": "Community Event",
+                "u": "https://i.imgur.com/myvtj8x.png"
+            },
+            {
+                "d": 21,
+                "i": "https://i.imgur.com/zGm6lA5.png",
+                "u": "https://i.imgur.com/9BgHnmP.jpg",
+                "g": "https://www.sg2global.com/forum/index.php?thread/367-event-moonlight-treasure-box/",
+                "t": "All Day",
+                "n": "Moonlight Box"
+            },
+            {
+                "d": 22,
+                "e": [{
+                    "i": "https://i.imgur.com/nnJgE4E.png",
+                    "g": "https://www.sg2global.com/forum/index.php?thread/551-event-hexagonal-box/",
+                    "t": "All Day",
+                    "n": "Hexagonal Box",
+                    "u": "https://i.imgur.com/kgPPMkZ.jpg"
+                }, {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "17:00",
+                    "n": "Nation War"
+                }]
+            },
+            {
+                "d": 23,
+                "i": "https://i.imgur.com/dAm7jFf.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/326-event-mining-party/",
+                "t": "17:00",
+                "n": "Mining Party",
+                "u": "https://i.imgur.com/JOiieaI.jpg",
+                "f": 0
+            },
+            {
+                "d": 24,
+                "i": "https://i.imgur.com/IllUyVq.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                "t": "17:00",
+                "n": "OX Event",
+                "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                "f": 0
+            },
+            {
+                "d": 25,
+                "i": "https://i.imgur.com/M015dld.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/365-event-dt-run/",
+                "t": "17:00",
+                "n": "DT Run",
+                "u": "https://i.imgur.com/l9rRJLi.jpg"
+            },
+            {
+                "d": 26,
+                "e": [
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "15:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    },
+                    {
+                        "i": "https://i.imgur.com/IllUyVq.png",
+                        "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                        "t": "17:00",
+                        "n": "OX Event",
+                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                        "f": 0
+                    }]
+            },
+            {
+                "d": 27,
+                "i": "./images/logo.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/562-event-boss-spawn/",
+                "t": "20:00",
+                "n": "Boss Spawn"
+            },
+            {
+                "d": 28,
+                "i": "https://i.imgur.com/zGm6lA5.png",
+                "u": "https://i.imgur.com/9BgHnmP.jpg",
+                "g": "https://www.sg2global.com/forum/index.php?thread/367-event-moonlight-treasure-box/",
+                "t": "All Day",
+                "n": "Moonlight Box"
+            },
+            {
+                "d": 29,
+                "i": "https://i.imgur.com/5HTFviS.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/372-event-mistery-box/",
+                "t": "All Day",
+                "n": "Mystery Box",
+                "u": "https://i.imgur.com/kB9HwLy.png"
+            },
+            {
+                "d": 30,
+                "i": "https://i.imgur.com/M015dld.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/365-event-dt-run/",
+                "t": "17:00",
+                "n": "DT Run",
+                "u": "https://i.imgur.com/l9rRJLi.jpg"
+            },
+            {
+                "d": 31,
+                "i": "https://i.imgur.com/IllUyVq.png",
+                "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
+                "t": "17:00",
+                "n": "OX Event",
+                "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                "f": 0
             }
         ]
     }
