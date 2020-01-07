39f64a967c4003e9d98f4e288a9edbb4e441f5d5
SG2Calendar <55944320+SG2Calendar@users.noreply.github.com>
2020-01-07 13:53:33 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1060,7 +1060,7 @@ var jsonData = {
                 "d": 7,
                 "i": "https://i.imgur.com/IllUyVq.png",
                 "g": "https://www.sg2global.com/forum/index.php?thread/321-event-ox-event/",
-                "t": "17:00",
+                "t": "20:00",
                 "n": "OX Event",
                 "u": "https://i.imgur.com/Kc7IsVi.jpg",
                 "f": 0
